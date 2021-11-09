b9dc581cfe858a46c54d4e85bc70037e101ef010
Jack <jackdewinter1@gmail.com>
2021-11-09 13:16:27 +0000

    Move to axois and refactor

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const request = require('request');
+const axios = require('axios');
 
 async function run() {
-    const { stdout, stderr } = await exec('docker run --rm -e "TESSDATA_PREFIX=/app" -v "$PWD/data":/app -w /app clearlinux/tesseract-ocr tesseract items.png stdout');
-    var results = stdout.substring(0, stdout.length - 2).split('\n');
-    var items = results.filter(x => x !== '');
-    getItemId(items[0]);
-    console.log(items);
+    const items = await getItemNames();
+    const itemIds = await getItemIds(items);
+    console.log(itemIds);
 }
 
-async function getItemId(item) {
-    request(`https://xivapi.com/search?indexes=Item&string=${item}`, { json: true}, (err, res, body) => {
-        if (err) { throw err; }
-        console.log(body.Results[0].ID);
-    });
+async function getItemNames() {
+    const { stdout, stderr } = await exec('docker run --rm -e "TESSDATA_PREFIX=/app" -v "$PWD/data":/app -w /app clearlinux/tesseract-ocr tesseract items.png stdout');
+    var results = stdout.substring(0, stdout.length - 2).split('\n');
+    return results.filter(x => x !== '');
+}
+
+function getItemIds(items) {
+    return Promise.all(items.map(getItemId)).then((results) => {
+        const ids = results.map(result => result.data.Results[0].ID);
+        return ids;
+    })
+}
+
+function getItemId(item) {
+    return axios.get(`https://xivapi.com/search?indexes=Item&string=${item}`);
 }
 
 run();
