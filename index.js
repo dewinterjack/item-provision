7a635a1ba7db2c5db9c6ad462061794111e1df79
Jack <jackdewinter1@gmail.com>
2021-11-09 12:30:16 +0000

    Call XIVAPI and log an ID

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const request = require('request');
 
 async function run() {
     const { stdout, stderr } = await exec('docker run --rm -e "TESSDATA_PREFIX=/app" -v "$PWD/data":/app -w /app clearlinux/tesseract-ocr tesseract items.png stdout');
     var results = stdout.substring(0, stdout.length - 2).split('\n');
     var items = results.filter(x => x !== '');
+    getItemId(items[0]);
     console.log(items);
+}
+
+async function getItemId(item) {
+    request(`https://xivapi.com/search?indexes=Item&string=${item}`, { json: true}, (err, res, body) => {
+        if (err) { throw err; }
+        console.log(body.Results[0].ID);
+    });
 }
 
 run();
